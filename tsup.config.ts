439b4e98bebe89650b87dcf0e1e49b3f9665462e
robinmin <luonbin@hotmail.com>
2025-10-19 00:19:27 -0700

    ci: add new packages and new scripts

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   clean: true,
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/cli/index.ts"],
   format: ["cjs", "esm"],
   sourcemap: true,
   dts: true,
+  cli: {
+    entry: "src/cli/index.ts",
+    target: "node14",
+    format: "cjs",
+  },
 })
