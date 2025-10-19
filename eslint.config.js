dde7ea2e5c67c6fe34367923b68c20b3b0207313
robinmin <luonbin@hotmail.com>
2025-10-19 09:03:21 -0700

    fix: resolve ESLint Git Commit Hook Issue

--- eslint.config.js
+++ eslint.config.js
@@ -8,8 +8,5 @@ export default defineConfig({
   files: ["**/*.{js,ts,cjs}"],
   rules: {
     "no-console": "off",
-    // Disable unicorn/error-message due to incompatibility with current ESLint version
-    // See: https://github.com/sindresorhus/eslint-plugin-unicorn/issues/2497
-    "unicorn/error-message": "off",
   },
 })
